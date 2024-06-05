8a677e2634ba50d643092622e0e7b8553f97dcee
Sebastian Hegge <github@heg.ge>
2024-06-05 12:51:35 +0200

    optimize script feedback, add NodeJS 22 (later LTS)

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "This script installs NodeJS"
+echo "This script installs NodeJS from it's own repositories"
 read -p "Do you want to continue? [Y/n] " yn
 case $yn in
   [yY] ) ;;
@@ -10,29 +10,38 @@ esac
 echo "Please select a NodeJS Major-Version:"
 echo "NodeJS 18 LTS [18]"
 echo "NodeJS 20 LTS [Enter, default] *"
+echo "NodeJS 22 (will later be LTS) [22]"
 read -p "NodeJS 21 [21] " selector
 case $selector in
   [18] ) NODE_MAJOR=18;;
   [21] ) NODE_MAJOR=21;;
+  [22] ) NODE_MAJOR=22;;
   * ) NODE_MAJOR=20;;
 esac
-
 echo "Major Version set to $NODE_MAJOR";
+
 echo "apt update is done...";
 apt update
+echo "apt update done!";
 
 read -p "Do you want to do a dist-upgrade first? [Y/n] " yn
 case $yn in
   [yY] )
     echo "dist-upgrade is done...";
-    apt dist-upgrade -y;;
+    apt dist-upgrade -y;
+    echo "dist-upgrade done!";;
 esac
 
-read -p "Do you want to install curl, htop, pwgen, zip, unzip, whois, ack-grep, net-tools and dnsutils? [Y/n] " yn
+read -p "Do you want to install curl, htop, pwgen, zip, unzip, whois, net-tools and dnsutils? [Y/n] " yn
 case $yn in
   [yY] )
     echo "packages are installed...";
-    apt install -y curl htop pwgen zip unzip whois ack-grep net-tools dnsutils;;
+    for package in curl htop pwgen zip unzip whois net-tools dnsutils; do
+      if ! dpkg -l "$package" | grep -q ^ii; then
+        apt install -y "$package"
+      fi
+    done;
+    echo "installation done!";;
 esac
 
 echo "gnupg, ca-certificates and apt-transport-https are installed..."
@@ -41,16 +50,21 @@ for package in gnupg ca-certificates apt-transport-https; do
     apt install -y "$package"
   fi
 done
+echo "installation done!"
 
 echo "NodeJS repo key is downloaded and added..."
 wget -qO - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/nodesource.gpg
 echo "deb https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "download and adding done!";
 
 echo "apt update is done (again)...";
 apt update
+echo "apt update done!";
 
 echo "NodeJS is installed...";
 apt install -y nodejs
+echo "installation done!";
 
 echo "corepack is enabled, is required, among other things, for yarn ...";
 corepack enable
+echo "enabling done!";
